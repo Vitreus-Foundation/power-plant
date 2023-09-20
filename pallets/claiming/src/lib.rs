142581a41ab860cad6e723fd1a07c902c3d4a9b8
Dmitry <dzmitry.rybakou@innowise-group.com>
2023-09-20 16:57:37 +0200

    Update claiming pallet

--- pallets/claiming/src/lib.rs
+++ pallets/claiming/src/lib.rs
@@ -6,12 +6,10 @@
 #![warn(clippy::all)]
 
 use crate::weights::WeightInfo;
-use frame_support::{
-    dispatch::DispatchResultWithPostInfo,
-    pallet_prelude::*,
-    traits::{Currency, EnsureOrigin, ExistenceRequirement::KeepAlive, ReservableCurrency},
-};
-use frame_system::ensure_root;
+use frame_support::{pallet_prelude::MaxEncodedLen, pallet_prelude::*};
+use pallet_atomic_swap::SwapAction;
+use scale_info::prelude::vec::Vec;
+use sp_core::H256;
 
 pub use pallet::*;
 
@@ -22,30 +20,38 @@ mod tests;
 
 pub mod weights;
 
+/// Information about tokens claiming.
+#[derive(Clone, Eq, PartialEq, RuntimeDebugNoBound, Encode, Decode, TypeInfo, MaxEncodedLen)]
+#[scale_info(skip_type_params(T))]
+#[codec(mel_bound())]
+pub struct ClaimingInfo<T: Config> {
+    /// Action of this claim.
+    pub action: T::SwapAction,
+    /// Ethereum transaction hash.
+    pub eth_hash: H256,
+}
+
 #[frame_support::pallet]
 pub mod pallet {
     use super::*;
     use frame_system::pallet_prelude::*;
 
     #[pallet::pallet]
+    #[pallet::without_storage_info]
     pub struct Pallet<T>(_);
 
     #[pallet::config]
-    pub trait Config: frame_system::Config {
+    pub trait Config: frame_system::Config + pallet_atomic_swap::Config {
         /// The overarching event type.
         type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
-
-        /// The origin which can manage less critical staking parameters that does not require root.
-        ///
-        /// Supported action: (1) assign token amount.
-        type AdminOrigin: EnsureOrigin<Self::RuntimeOrigin>;
-
-        /// The currency mechanism, used for amount transferring.
-        type Currency: ReservableCurrency<Self::AccountId>;
 
         /// Weight information for extrinsic.
         type WeightInfo: WeightInfo;
     }
+
+    #[pallet::storage]
+    pub type Claims<T: Config> =
+        StorageMap<_, Blake2_128Concat, T::AccountId, Vec<ClaimingInfo<T>>, ValueQuery>;
 
     #[pallet::event]
     #[pallet::generate_deposit(pub(super) fn deposit_event)]
@@ -54,58 +60,60 @@ pub mod pallet {
         Claimed {
             /// To whom the tokens were claimed.
             account_id: T::AccountId,
-            /// Number of tokens.
-            amount: <T::Currency as Currency<T::AccountId>>::Balance,
+            /// Swap information.
+            swap_info: ClaimingInfo<T>,
         },
 
-        /// Tokens was assigned to account (by root).
-        TokenAssigned {
-            /// To whom the tokens were assigned.
+        /// User claims information.
+        ClaimsInfo {
+            /// About whom the claims information was issued.
             account_id: T::AccountId,
-            /// Number of tokens.
-            amount: <T::Currency as Currency<T::AccountId>>::Balance,
+            /// Information about claims: value and ethereum transaction hash.
+            claims: Vec<ClaimingInfo<T>>,
         },
     }
 
     #[pallet::error]
     pub enum Error<T> {
-        /// Error when transferring tokens for a user
+        /// Error when claiming tokens for a user
         ClaimProcessError,
     }
 
     #[pallet::call]
     impl<T: Config> Pallet<T> {
-        /// Assign tokens to account.
-        #[pallet::call_index(0)]
-        #[pallet::weight(<T as Config>::WeightInfo::assign_token_amount())]
-        pub fn assign_token_amount(
-            origin: OriginFor<T>,
-            account_id: T::AccountId,
-            amount: <T::Currency as Currency<T::AccountId>>::Balance,
-        ) -> DispatchResultWithPostInfo {
-            T::AdminOrigin::try_origin(origin).map(|_| ()).or_else(ensure_root)?;
-
-            <T::Currency as Currency<T::AccountId>>::deposit_creating(&account_id, amount);
-
-            Self::deposit_event(Event::TokenAssigned { account_id, amount });
-
-            Ok(().into())
-        }
-
         /// Claim tokens to user account.
-        #[pallet::call_index(1)]
+        #[pallet::call_index(0)]
         #[pallet::weight(<T as Config>::WeightInfo::claim())]
         pub fn claim(
             origin: OriginFor<T>,
             account_id: T::AccountId,
-            amount: <T::Currency as Currency<T::AccountId>>::Balance,
-        ) -> DispatchResultWithPostInfo {
-            let who = ensure_signed(origin)?;
+            action: T::SwapAction,
+            eth_hash: H256,
+        ) -> DispatchResult {
+            let source = ensure_signed(origin)?;
 
-            Self::process_claim(who, &account_id, amount)?;
-            Self::deposit_event(Event::Claimed { account_id, amount });
+            action.reserve(&source)?;
 
-            Ok(().into())
+            let swap = ClaimingInfo::<T> { action, eth_hash };
+
+            Self::process_claim(swap.clone(), &source, &account_id)?;
+
+            Self::deposit_event(Event::<T>::Claimed { account_id, swap_info: swap });
+
+            Ok(())
+        }
+
+        /// Get user claims information.
+        #[pallet::call_index(1)]
+        #[pallet::weight(<T as Config>::WeightInfo::get_claims_info())]
+        pub fn get_claims_info(origin: OriginFor<T>, account_id: T::AccountId) -> DispatchResult {
+            ensure_signed(origin)?;
+
+            let claims = Claims::<T>::get(&account_id);
+
+            Self::deposit_event(Event::<T>::ClaimsInfo { account_id, claims });
+
+            Ok(())
         }
     }
 }
@@ -113,11 +121,18 @@ pub mod pallet {
 impl<T: Config> Pallet<T> {
     /// Claims tokens to account wallet.
     fn process_claim(
-        who: T::AccountId,
-        account_id: &T::AccountId,
-        amount: <T::Currency as Currency<T::AccountId>>::Balance,
+        swap_info: ClaimingInfo<T>,
+        source: &T::AccountId,
+        to_acc: &T::AccountId,
     ) -> DispatchResult {
-        <T::Currency as Currency<T::AccountId>>::transfer(&who, account_id, amount, KeepAlive)?;
+        if !swap_info.action.claim(source, to_acc) {
+            return Err(Error::<T>::ClaimProcessError)?;
+        }
+
+        let mut claims = Claims::<T>::get(to_acc);
+        claims.push(swap_info);
+        Claims::<T>::insert(to_acc, claims);
+
         Ok(())
     }
 }
