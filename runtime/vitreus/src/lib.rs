f1dfb3431b1a1c4636bdc48b8c0de7057ae99a17
Alexander Pastushenko <alexander.pastushenko@innowise.com>
2024-03-31 22:29:44 +0400

    Set spec version 100

--- runtime/vitreus/src/lib.rs
+++ runtime/vitreus/src/lib.rs
@@ -218,7 +218,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("vitreus-power-plant"),
     impl_name: create_runtime_str!("vitreus-power-plant"),
     authoring_version: 1,
-    spec_version: 8,
+    spec_version: 100,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
