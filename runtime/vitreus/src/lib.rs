1a97316a67d0cf15464843d15712feaaf8d2d78a
Dzmitry Rybakou <dmitri.safe@gmail.com>
2024-03-31 15:05:47 +0200

    Update spec_version

--- runtime/vitreus/src/lib.rs
+++ runtime/vitreus/src/lib.rs
@@ -217,7 +217,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("vitreus-power-plant"),
     impl_name: create_runtime_str!("vitreus-power-plant"),
     authoring_version: 1,
-    spec_version: 6,
+    spec_version: 8,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
