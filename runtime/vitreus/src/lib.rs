412ada0d08eb605459046b0a460d8790760e1af8
Alexander Pastushenko <alexander.pastushenko@innowise.com>
2024-04-12 12:51:20 +0400

    Fix try-runtime

--- runtime/vitreus/src/lib.rs
+++ runtime/vitreus/src/lib.rs
@@ -2358,4 +2358,24 @@ impl_runtime_apis! {
             Err(sp_mmr_primitives::Error::PalletNotIncluded)
         }
     }
+
+    #[cfg(feature = "try-runtime")]
+    impl frame_try_runtime::TryRuntime<Block> for Runtime {
+        fn on_runtime_upgrade(checks: frame_try_runtime::UpgradeCheckSelect) -> (Weight, Weight) {
+            log::info!("try-runtime::on_runtime_upgrade");
+            let weight = Executive::try_runtime_upgrade(checks).unwrap();
+            (weight, BlockWeights::get().max_block)
+        }
+
+        fn execute_block(
+            block: Block,
+            state_root_check: bool,
+            signature_check: bool,
+            select: frame_try_runtime::TryStateSelect,
+        ) -> Weight {
+            // NOTE: intentional unwrap: we don't want to propagate the error backwards, and want to
+            // have a backtrace here.
+            Executive::try_execute_block(block, state_root_check, signature_check, select).unwrap()
+        }
+    }
 }
