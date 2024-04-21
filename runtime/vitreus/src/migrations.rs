134b6e42ee924debc24d4b3bcd1734defceef107
Dzmitry Rybakou <dmitri.safe@gmail.com>
2024-04-21 19:46:50 +0200

    Fix rewards calculation, cargo clippy && fmt

--- runtime/vitreus/src/migrations.rs
+++ runtime/vitreus/src/migrations.rs
@@ -92,12 +92,10 @@ impl OnRuntimeUpgrade for FixRewards {
                     weight += RocksDbWeight::get().reads_writes(4, 3);
                     if pallet_nac_managing::UsersNft::<Runtime>::contains_key(account.0) {
                         log::info!("User {:?} already has NAC (2 level)", account.0);
+                    } else if NacManaging::mint(RawOrigin::Root.into(), 1, account.0).is_err() {
+                        log::warn!("NacManaging::mint call failed for {:?}", account.0);
                     } else {
-                        if NacManaging::mint(RawOrigin::Root.into(), 1, account.0).is_err() {
-                            log::warn!("NacManaging::mint call failed for {:?}", account.0);
-                        } else {
-                            log::info!("Mint NAC (1 level) to {:?}", account.0);
-                        }
+                        log::info!("Mint NAC (1 level) to {:?}", account.0);
                     }
                 }
 
