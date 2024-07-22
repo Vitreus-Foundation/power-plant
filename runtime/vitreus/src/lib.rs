75fd85095fd2fb31b9456be003b55e6b97cafc3d
Dzmitry Rybakou <dmitri.safe@gmail.com>
2024-07-22 14:41:13 +0300

    Update 'MaxAuthorities' const

--- runtime/vitreus/src/lib.rs
+++ runtime/vitreus/src/lib.rs
@@ -408,7 +408,7 @@ impl pallet_grandpa::Config for Runtime {
     type RuntimeEvent = RuntimeEvent;
 
     type WeightInfo = ();
-    type MaxAuthorities = ConstU32<32>;
+    type MaxAuthorities = ConstU32<1024>;
     type MaxSetIdSessionEntries = ConstU64<168>;
 
     type KeyOwnerProof = <Historical as KeyOwnerProofSystem<(KeyTypeId, GrandpaId)>>::Proof;
