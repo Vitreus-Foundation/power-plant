42753b5bfbb360ae5513a616ef5695b6b94d9c55
Alexander Pastushenko <a.pastushenka@gmail.com>
2025-01-06 18:29:28 +0400

    Runtime version 208

--- runtime/vitreus/src/migrations.rs
+++ runtime/vitreus/src/migrations.rs
@@ -25,7 +25,15 @@ pub type V0200 = (
 
 pub type V0205 = (claiming::RemoveStorage);
 
-pub type Unreleased = (
+parameter_types! {
+    pub const EnergyBrokerCapacity: u128 = 10_000_000_000_000_000;
+    pub const EnergyBurn: u128 = 2_009_132_407_194;
+    pub const EnergySale: u128 = 1_642_710_472_279;
+    pub const TotalStake: u128 = 160000000000000000000000000;
+    pub EnergyExchangeRate: FixedU128 = FixedU128::from_rational(900_000_000, 1_000_000_000_000_000_000);
+}
+
+pub type V0208 = (
     energy_broker::MigrateToEnergyBrokerV2,
     pallet_energy_broker::migration::MigrateToV1<Runtime, EnergyBrokerCapacity>,
     pallet_energy_generation::migrations::v16::MigrateV15ToV16<Runtime>,
@@ -39,13 +47,7 @@ pub type Unreleased = (
     >,
 );
 
-parameter_types! {
-    pub const EnergyBrokerCapacity: u128 = 10_000_000_000_000_000;
-    pub const EnergyBurn: u128 = 2_009_132_407_194;
-    pub const EnergySale: u128 = 1_642_710_472_279;
-    pub const TotalStake: u128 = 160000000000000000000000000;
-    pub EnergyExchangeRate: FixedU128 = FixedU128::from_rational(900_000_000, 1_000_000_000_000_000_000);
-}
+pub type Unreleased = ();
 
 mod energy_broker {
     use super::*;
