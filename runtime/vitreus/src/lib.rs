43e5bb26cdd60108235e0faf4a3d113ae1c95138
Alexander Pastushenka <a.pastushenka@gmail.com>
2025-03-06 01:00:30 +0400

    Runtime version 212 (#78)

--- runtime/vitreus/src/lib.rs
+++ runtime/vitreus/src/lib.rs
@@ -253,7 +253,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("vitreus-power-plant"),
     impl_name: create_runtime_str!("vitreus-power-plant"),
     authoring_version: 1,
-    spec_version: 211,
+    spec_version: 212,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 4,
